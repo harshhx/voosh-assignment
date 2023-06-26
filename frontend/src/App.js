410129263145c834bd799665f43f032ce96edb93
harshhx <hg070401@gmail.com>
2023-06-26 23:56:25 +0530

    basic login and register comp setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import SignIn from "./components/Login";
+import SignUp from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [register, setRegister] = useState(true);
+  return <div className="App">{register ? <SignUp setRegister={setRegister} /> : <SignIn setRegister={setRegister}/>}</div>;
 }
 
 export default App;
